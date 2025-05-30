d8645c0c88b9e3f6353d43a87c3eadb35d80bd09
McVITIES755 <mcvities755@gmail.com>
2025-05-30 21:50:46 +0100

    commit
    SIXTH COMMIT$$$

--- src/index.js
+++
@@ -1,8 +0,0 @@
-addEventListener('fetch', event => {
-    event.respondWith(
-      new Response('Hello from Cloudflare Worker!', {
-        headers: { 'content-type': 'text/plain' }
-      })
-    )
-  })
-  
