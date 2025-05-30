27ee636401c4a98b2620af4c3619f84f983a1612
McVITIES755 <mcvities755@gmail.com>
2025-05-30 21:06:06 +0100

    commit
    SECOND COMMIT$$$

---
+++ index.js
@@ -0,0 +1,8 @@
+addEventListener('fetch', event => {
+    event.respondWith(
+      new Response('Hello from Cloudflare Worker!', {
+        headers: { 'content-type': 'text/plain' }
+      })
+    )
+  })
+  
